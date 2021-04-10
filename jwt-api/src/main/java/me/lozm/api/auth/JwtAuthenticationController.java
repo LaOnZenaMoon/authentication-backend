d9602769d153381bd88f61a7447374d844237451
junlee <laonzenamoon@gmail.com>
2021-04-10 15:06:41 +0900

    feat: Swagger 적용

--- jwt-api/src/main/java/me/lozm/api/auth/JwtAuthenticationController.java
+++ jwt-api/src/main/java/me/lozm/api/auth/JwtAuthenticationController.java
@@ -1,5 +1,7 @@
 package me.lozm.api.auth;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import me.lozm.domain.auth.dto.AuthDto;
 import me.lozm.domain.auth.vo.AuthVo;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = {"JWT 인증"})
 @RestController
 @RequiredArgsConstructor
 public class JwtAuthenticationController {
@@ -15,8 +18,9 @@ public class JwtAuthenticationController {
     private final JwtAuthenticationService jwtAuthenticationService;
 
 
+    @ApiOperation("JWT 인증토큰 발급")
     @PostMapping("authenticate")
-    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthDto.Request reqDto) {
+    public ResponseEntity createAuthenticationToken(@RequestBody AuthDto.Request reqDto) {
         AuthVo jwt = jwtAuthenticationService.getToken(AuthVo.builder()
                 .identifier(reqDto.getIdentifier())
                 .password(reqDto.getPassword())
